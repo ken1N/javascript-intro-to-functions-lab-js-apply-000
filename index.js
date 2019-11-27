b1206e7003c88f9a9dfb31ab20a4ab128eff2da1
Kenny Choi <kkenc.gg@gmail.com>
2019-11-27 00:13:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function logShout(string){
   return console.log(shout(string));
 }
 
+function whisper(string){
+  return string.toLowerCase();
+}
 function logWhisper(string){
-  return console.log(string.toLowerCase());
+  return console.log(whisper(string));
 }
